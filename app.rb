a2629c8543023af54243e894625e29a3ef0f4f87
Chetan Sanghani <crsanghani@gmail.com>
2016-10-11 09:42:10 +0100

    chapter 14 completed

--- app.rb
+++ app.rb
@@ -23,7 +23,11 @@ set :session_secret, 'super secret', 'very secret'
     erb(:index)
   end
 
-  get '/named-cat' do
+  get '/cat-form' do
+      erb :cat_form
+  end
+
+  post '/named-cat' do
     p params
     @name = params[:name]
     erb :index
